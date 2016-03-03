164ecf1d14a7ba15d7c33dfb69aa89eed1382bd0
Margus Sumla <msumla@itcollege.ee>
2016-03-03 15:01:54 +0200

    Added GeoIP function and top requested IP list

--- parse_logs.py
+++ parse_logs.py
@@ -2,14 +2,27 @@ import os
 import gzip
 from readlog import Readlog
 readlog = Readlog()
+#from argparser import Argparser
+#argparser = Argparser()
 i = 0
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Apache2 log parser')
+parser.add_argument('--path', help='Path to Apache2 log files', default="logs")
+parser.add_argument('--top-urls', help="Find top URL-s", action='store_true')
+parser.add_argument('--geoip', help="Resolve IP-s to country codes", action='store_true')
+
+args = parser.parse_args()
+print("We are expecting logs from:", args.path)
+print("Do we want top URL-s?")
  
 # Following is the directory with log files,
 # On Windows substitute it where you downloaded the files
 root = "logs"
 
 print("Parsing ..")
-for filename in os.listdir(root):
+for filename in os.listdir(args.path):
     if not filename.startswith("access.log"):
 #        print "Skipping unknown file:", filename
         continue
@@ -23,6 +36,11 @@ for filename in os.listdir(root):
 
 print "Log files parsed: ", i
 
-res = readlog.users.items()
+#res = readlog.users.items()
+#res.sort(key = lambda item:item[1], reverse=True)
+#print(res[:1])
+
+res = readlog.ips.items()
 res.sort(key = lambda item:item[1], reverse=True)
-print(res[:1])
+for o in range(3):
+    print("Top ", o+1, res[o])
--- readlog.py
+++ readlog.py
@@ -1,39 +1,47 @@
-class Readlog(object):
+class Readlog():
 
     def readLog(self, fh):
 
         import urllib
+        import GeoIP
+        
 #        fh = open("access.log")
-
+        gi = GeoIP.open("GeoIP.dat", GeoIP.GEOIP_MEMORY_CACHE)
         keywords = "Windows", "Linux", "Mac", "Googlebot"
         d = {"Windows":0, "Linux":0, "Mac":0, "Googlebot":0} #a dictionary aka an array
         self.users = {}
+        self.ips = {}
         win = 0
         total = 0
-
+		
         for line in fh: #for each
             total = total + 1
             try:
                 source_timestamp, request, response, _, _, agent, _ = line.split("\"") #split by "
                 method, path, protocol = request.split(" ") #split by space and define as variables
                 path = urllib.unquote(path)
-                if path.startswith("/~"):
-                    username, remainder = path[2:].split("/", 1)
-                    try:
-                        self.users[username] += 1
-                    except:
-                        self.users[username] = 1
+                
+                ip_address, _ , _, timestamp = source_timestamp.split(" ", 3)
+                if not ":" in ip_address:
+                    self.ips[ip_address] = self.ips.get(ip_address, 0) + 1
+                    print(ip_address, " @ ", gi.country_code_by_addr(ip_address))
+#                if path.startswith("/~"):
+#                    username, remainder = path[2:].split("/", 1)
+#                    try:
+#                        self.users[username] += 1
+#                    except:
+#                        self.users[username] = 1
 
 #                for keyword in keywords: #for each
 #                    if keyword in agent: #ctrl+f
 #                        d[keyword] += 1
 #                        break
 #                print(path)
-        #        browser, osetc, _, _, _ = agent.split(";")
-        #        print osetc
+#                 browser, osetc, _, _, _ = agent.split(";")
+#                 print osetc
 #                if "Windows" in agent: #ctrl+f
 #                    win += 1
-            except ValueError:
+            except ValueError, KeyError:
                 pass #mandatory syntax, doesn't do anthing
               
 
